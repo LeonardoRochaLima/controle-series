d70cab017e4022d7a3c3598c2ca927a23c7532eb
Leonardo Vinícius Rocha de Lima <leonardo.lima@R3213.corp.rentcars.com>
2023-06-01 10:39:30 -0300

    Testing a database conection to save something

--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,6 +1,10 @@
 <x-layout title="Adicionar Série">
-    <form action="" method="POST">
-        <label for="nome">Nome: </label>
-        <input type="text" id="nome" name="nome">
+    <form action="/series/salvar" method="POST">
+        <div>
+            <label for="nome" class="form-label">Nome: </label>
+            <input type="text" id="nome" name="nome" class="form-control">
+        </div>
+
+        <button type="submit" class="btn btn-primary">Adicionar</button>
     </form>
 </x-layout>
