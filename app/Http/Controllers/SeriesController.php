1b9e2ef8b9773365433fdd48806d045d3ca3022a
Leonardo Vinícius Rocha de Lima <leonardo.lima@R3213.corp.rentcars.com>
2023-06-01 11:03:54 -0300

    Updating the version of Laravel to the same version of the course, to use Laravel Mix

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -3,16 +3,15 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class SeriesController extends Controller
 {
-    public function index()
+    public function index(Request $request)
     {
         $series = [
             'Punisher',
             'Lost',
-            'Gray\'s Anatomy'
+            'Grey\'s Anatomy',
         ];
 
         return view('series.index')->with('series', $series);
@@ -21,15 +20,5 @@ class SeriesController extends Controller
     public function create()
     {
         return view('series.create');
-    }
-
-    public function store(Request $request)
-    {
-        $nomeSerie = $request->input('nome');
-        if(DB::insert('INSERT INTO series (nome) VALUES(?)', [$nomeSerie])){
-            return 'OK';
-        }else{
-            return "Deu erro";
-        }
     }
 }
--- resources/views/series/create.blade.php
+++ resources/views/series/create.blade.php
@@ -1,10 +1,6 @@
-<x-layout title="Adicionar Série">
-    <form action="/series/salvar" method="POST">
-        <div>
-            <label for="nome" class="form-label">Nome: </label>
-            <input type="text" id="nome" name="nome" class="form-control">
-        </div>
-
-        <button type="submit" class="btn btn-primary">Adicionar</button>
+<x-layout title="Nova Série">
+    <form action="" method="post">
+        <label for="nome">Nome:</label>
+        <input type="text" id="nome" name="nome">
     </form>
 </x-layout>
